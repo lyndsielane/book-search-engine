da86fba33b36df9ea84d3122c49ae3bfba6c4f7e
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-23 15:59:38 -0400

    trying to deploy

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <Router>
