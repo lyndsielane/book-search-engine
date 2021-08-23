17e1bb6464c1efe70f74169e5f48daa61359811a
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-23 19:50:16 -0400

    added missing path import

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
