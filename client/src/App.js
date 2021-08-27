de62c61b80cb9be4b4141ed8b5141837e20c6b10
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-27 17:54:35 -0400

    nope... far from fixed

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,37 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-/*
+
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
-});*/
+});
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 function App() {
   return (
+  <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -22,6 +44,7 @@ function App() {
         </Switch>
       </>
     </Router>
+  </ApolloProvider>
   );
 }
 
